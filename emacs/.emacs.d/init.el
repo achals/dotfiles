136866cb72e15a375c67488c004f3606c37d2a5d
Achal Shah <achals@gmail.com>
2015-04-14 19:37:02 -0700

    Adding the .emacs.d files correctly.

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,71 @@
+;; .emacs
+
+;;; uncomment this line to disable loading of "default.el" at startup
+;; (setq inhibit-default-init t)
+
+;; enable visual feedback on selections
+;(setq transient-mark-mode t)
+
+;; default to better frame titles
+(setq frame-title-format
+      (concat  "%b - emacs@" (system-name)))
+
+;; default to unified diffs
+(setq diff-switches "-u")
+
+;; always end a file with a newline
+;(setq require-final-newline 'query)
+
+;;; uncomment for CJK utf-8 support for non-Asian users
+;; (require 'un-define)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(indent-tabs-mode nil)
+)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(defvar my-packages '(better-defaults
+                      projectile
+                      clojure-mode
+                      cider
+                      color-theme
+                      python-mode))
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(let ((default-directory "~/.emacs.d/lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-dark-laptop)
+
+(require 'python-mode)
+
+(setq-default tab-width 4)
+(defun my-c++-indent-setup ()
+  (setq c-basic-offset 4)
+  (setq indent-tabs-mode nil))
+
+;; regular auto-complete initialization
+(require 'auto-complete-config)
+(ac-config-default)
