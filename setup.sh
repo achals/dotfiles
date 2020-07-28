f758cf20a8a4d5615f500f0bafa61883618eaae1
Achal Shah <achal@tecton.ai>
2020-07-28 16:47:36 -0700

    Add zsh autocompletion and syntax plugins

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 stow bash -t $HOME
 stow git -t $HOME
 stow zsh -t $HOME
+
+# Taken from https://github.com/zellwk/dotfiles/blob/master/install-zsh.sh
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+# Installs plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Fix permissions
+chmod 700 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
